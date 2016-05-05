b88b140ebfd8fdb09b41f34aaf7abe31c90a9559
Malin Soeun <malinhok@gmail.com>
2016-05-05 13:31:40 -0500

    Initial Commit

---
+++ anotate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  anotate
+//
+//  Created by Malin on 2016-05-05.
+//  Copyright © 2016 Malin Soeun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ anotate/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  anotate
+//
+//  Created by Malin on 2016-05-05.
+//  Copyright © 2016 Malin Soeun. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
